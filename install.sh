2a51e41a27569f50b04c68fc6a4d2d6a94114094
XeroLinux <steve@techxero.com>
2022-06-26 01:35:54 +0300

    Update

--- install.sh
+++ install.sh
@@ -10,17 +10,16 @@ mv ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
 sleep 2
 echo "Removing old Latte-Dock & Lightly"
 echo "#################################"
-sudo pacman -R --noconfirm lightly-git
 sudo pacman -R --noconfirm latte-dock
 sleep 2
 echo
 echo "Installing New latte-Git version"
 echo "#################################"
-sudo pacman -S --needed --noconfirm latte-dock-git kvantum
+sudo pacman -S --needed --noconfirm latte-dock-git lightly-git
 sleep 2
-echo "Installing Layan Theme"
+echo "Installing Dracula Theme"
 echo "#################################"
-sudo pacman -S --needed --noconfirm layan-gtk-theme-git layan-kde-git tela-circle-icon-theme-git
+sudo pacman -S --needed --noconfirm ant-dracula-gtk-theme ant-dracula-kde-theme-git ant-dracula-kvantum-theme-git ant-dracula-theme-git kvantum dracula-cursors-git dracula-icons-git dracula-xresources-git
 sleep 2
 echo "Installing Fonts"
 echo "#################################"
@@ -29,15 +28,15 @@ sleep 2
 echo "Applying new Rice, hold on..."
 echo "#################################"
 cp -Rf Configs/Home/. ~
-sudo rm /usr/share/defaultbg/XeroLinux.png
 sudo cp -Rf Configs/System/. /
 sleep 2
-echo "Applying Grub Theme...."
+#echo "Applying Grub Theme...."
+#echo "#################################"
+#chmod +x CyberRe.sh
+#sudo ./CyberRe.sh
+#sudo sed -i "s/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=1920x1080/g" /etc/default/grub
+#sudo grub-mkconfig -o /boot/grub/grub.cfg
+#sleep 2
+echo "Rebooting To Apply Settings..."
 echo "#################################"
-chmod +x CyberRe.sh
-sudo ./CyberRe.sh
-sudo sed -i "s/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=1920x1080/g" /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-sleep 2
-echo "RebootTo Apply Settings..."
-echo "#################################"
+reboot
