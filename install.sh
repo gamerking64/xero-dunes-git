99f13af4a49a13a89fff863e002a26c011a9579c
XeroLinux <steve@techxero.com>
2022-07-03 12:48:08 +0300

    LightlyShaders

--- install.sh
+++ install.sh
@@ -15,11 +15,11 @@ sleep 2
 echo
 echo "Installing Lightly For best look"
 echo "###################################"
-sudo pacman -S --needed --noconfirm lightly-git nordic-wallpapers
+sudo pacman -S --needed --noconfirm lightly-git lightlyshaders-git
 sleep 2
 echo "Installing Dracula Theme"
 echo "#################################"
-sudo pacman -S --needed --noconfirm ant-dracula-theme-git ant-dracula-kde-theme-git ant-dracula-kvantum-theme-git kvantum dracula-cursors-git dracula-icons-git dracula-xresources-git
+sudo pacman -S --needed --noconfirm nordic-wallpapers ant-dracula-theme-git ant-dracula-kde-theme-git ant-dracula-kvantum-theme-git kvantum dracula-cursors-git dracula-icons-git dracula-xresources-git
 sleep 2
 echo "Installing Fonts"
 echo "#################################"
@@ -38,13 +38,6 @@ chmod +x Grub.sh
 sudo ./Grub.sh
 sudo sed -i "s/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=1920x1080/g" /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-sleep 2
-echo
-echo "Installing LightlyShaders rounded"
-echo "###################################"
-sudo pacman -S --needed --noconfirm git make cmake gcc gettext extra-cmake-modules qt5-tools qt5-x11extras kcrash kglobalaccel kde-dev-utils kio knotifications kinit kwin
-cd $HOME/ && git clone https://github.com/a-parhom/LightlyShaders
-cd LightlyShaders; mkdir qt5build; cd qt5build; cmake ../ -DCMAKE_INSTALL_PREFIX=/usr && make && sudo make install
 sleep 2
 echo
 echo "Rebooting To Apply Settings..."
